91f1970bb7c1d9247ca97ab90867b80d8d3191ac
sorin.lapadus <sorin.lapadus@indsoft.ro>
2023-07-31 21:03:23 +0300

    Add AppFooter

--- src/App.tsx
+++ src/App.tsx
@@ -36,8 +36,35 @@ function AppHeader() {
   );
 }
 
+function AppFooter() {
+  return (
+    <footer>
+      <a href="https://github.com/nmatei/node-api" target="_blank">
+        <span>📃 </span>
+        <span>API Docs</span>
+      </a>
+      |
+      <a href="https://github.com/sorinlapadus/teams-networking" target="_blank">
+        <span>👩&zwj;💻 </span>
+        <span>Source Code</span>
+      </a>
+    </footer>
+  );
+}
+
+function ContentWrapper() {
+  // TODO
+  return <div id="main"></div>;
+}
+
 function App() {
-  return <AppHeader />;
+  return (
+    <>
+      <AppHeader />
+      <ContentWrapper />
+      <AppFooter />
+    </>
+  );
 }
 
 export default App;
