93484337bacc7f732ba99a2a341cc17b5a0ee4a3
sorin.lapadus <sorin.lapadus@indsoft.ro>
2023-07-31 20:51:52 +0300

    Add App header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import logo from "./logo.svg";
+import logo from "./images/network-team-icon.png";
+import "./App.css";
+import "./style.css";
 
-function App() {
+function App_old() {
   return (
     <div className="App">
       <header className="App-header">
@@ -10,17 +12,32 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
   );
+}
+
+function AppHeader() {
+  return (
+    <div id="header-wrapper">
+      <header>
+        <div id="my-picture">
+          <img src={logo} alt="poza" width="100" />
+        </div>
+        <div>
+          <h1>Teams networking</h1>
+          <h2>CRUD operations example (Create, Read, Update, Delete)</h2>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+function App() {
+  return <AppHeader />;
 }
 
 export default App;
