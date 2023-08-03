00c3f0ef237129c07542b18bf2874aeb8cb9ef1a
sorin.lapadus <sorin.lapadus@indsoft.ro>
2023-08-03 17:20:40 +0300

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,6 @@ import logo from "./images/network-team-icon.png";
 import "./App.css";
 import "./style.css";
 
-function App_old() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
 function AppHeader() {
   return (
     <div id="header-wrapper">
