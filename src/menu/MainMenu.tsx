11e78a1705b5cfaa7a6a094982edef92b4ebc911
sorin.lapadus <sorin.lapadus@indsoft.ro>
2023-08-14 18:29:13 +0300

    MainMenu

--- src/menu/MainMenu.tsx
+++ src/menu/MainMenu.tsx
@@ -1,19 +1,40 @@
 import "./menu.css";
+import { useState } from "react";
 export function MainMenu() {
+  //const active: string = "home";
+  const [active, setActive] = useState("");
   return (
     <ul id="top-menu-bar">
       <li>
-        <a href="#" data-page="home">
+        <a
+          href="#home"
+          className={active === "home" ? "active" : ""}
+          onClick={() => {
+            setActive("home");
+          }}
+        >
           HOME
         </a>
       </li>
       <li>
-        <a href="#" data-page="TODO">
+        <a
+          href="#TODO"
+          className={active === "TODO" ? "active" : ""}
+          onClick={() => {
+            setActive("TODO");
+          }}
+        >
           TODO
         </a>
       </li>
       <li>
-        <a href="#" data-page="Teams">
+        <a
+          href="Teams"
+          className={active === "Teams" ? "active" : ""}
+          onClick={() => {
+            setActive("Teams");
+          }}
+        >
           Teams
         </a>
       </li>
